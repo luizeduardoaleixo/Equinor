184e38504b89df4803164a7d31bcc08c5320b8bb
Luiz Eduardo <luiz.eduardo.aleixo@accenture.com>
2022-10-14 11:26:00 -0300

    refactor: folder structure

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import MyMap from "./components/map/MyMap";
+import useFetchData from "./services/useFetchData";
+import useCitiesStore from "./store/store";
+import { Data } from "./types/data";
+
+const url =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
+
+function App(): JSX.Element {
+  const { data, isLoading, isError } = useFetchData(url);
+
+  const setCities = useCitiesStore((store) => store.setCities);
+  const setPopulationSum = useCitiesStore((store) => store.setPopulationSum);
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!isLoading && !isError) {
+      setCities(data);
+      const populationSum = data.reduce(
+        (acc: number, city: Data) => acc + parseInt(city.population),
+        0
+      );
+      setPopulationSum(populationSum);
+      console.log(populationSum, "populationSum");
+    }
+  }, [data]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MyMap></MyMap>
+      </header>
+    </div>
+  );
+}
+
+export default App;
